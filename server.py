24c06f677fc5c4724421a804fd18a84bf6bc100a
Md. Mahir Uddin <157863988+Mahir-Uddin0@users.noreply.github.com>
2025-09-13 00:04:13 +0600

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,16 @@
+from flask import Flask, request, render_template
+from EmotionDetection.emotion_detection import emotion_detector
 
+app = Flask("Emotion Detector")
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+@app.route("/emotionDetector")
+def emotion_analyzer():
+    pass
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
